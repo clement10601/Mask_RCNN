75b2f7108cf850a68c70926cfeafa9ffeaf2118b
kchwang <clement10601@gmail.com>
2018-02-02 16:06:14 +0800

    fix dir not exist problem

--- demo.py
+++ demo.py
@@ -22,12 +22,17 @@ ROOT_DIR = os.getcwd()
 # Directory to save logs and trained model
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 # Local path to trained weights file
+COCO_MODEL_DIR = os.path.join(ROOT_DIR, "weights")
+if not os.path.exists(COCO_MODEL_DIR):
+    os.makedirs(COCO_MODEL_DIR)
 COCO_MODEL_PATH = os.path.join(ROOT_DIR, "weights/mask_rcnn_coco.h5")
 # Download COCO trained weights from Releases if needed
 if not os.path.exists(COCO_MODEL_PATH):
     utils.download_trained_weights(COCO_MODEL_PATH)
 # Directory of images to run detection on
 IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+if not os.path.exists(IMAGE_DIR):
+    os.makedirs(IMAGE_DIR)
 class InferenceConfig(coco.CocoConfig):
     # Set batch size to 1 since we'll be running inference on
     # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
--- mrcnn.py
+++ mrcnn.py
@@ -33,6 +33,9 @@ ROOT_DIR = os.getcwd()
 # Directory to save logs and trained model
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 # Local path to trained weights file
+COCO_MODEL_DIR = os.path.join(ROOT_DIR, "weights")
+if not os.path.exists(COCO_MODEL_DIR):
+    os.makedirs(COCO_MODEL_DIR)
 COCO_MODEL_PATH = os.path.join(ROOT_DIR, "weights/mask_rcnn_coco.h5")
 # Download COCO trained weights from Releases if needed
 if not os.path.exists(COCO_MODEL_PATH):
