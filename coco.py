c07fec15c265f9979812810c71d95d5d27dd2507
john <ban77323@hotmail.com>
2018-02-09 02:45:56 +0000

    add path of cocoapi

--- coco.py
+++ coco.py
@@ -31,6 +31,11 @@ import os
 import time
 import numpy as np
 
+import sys
+
+sys.path.append('./cocoapi/PythonAPI')
+
+
 # Download and install the Python COCO tools from https://github.com/waleedka/coco
 # That's a fork from the original https://github.com/pdollar/coco with a bug
 # fix for Python 3.
