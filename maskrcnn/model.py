0bc9b9376a40f76be89384db0f19c183c5a1eb95
kcw <kcw@cht.com.tw>
2018-03-19 16:55:36 +0800

    fix parallel_model import route

--- maskrcnn/model.py
+++ maskrcnn/model.py
@@ -1975,7 +1975,7 @@ class MaskRCNN():
 
         # Add multi-GPU support.
         if config.GPU_COUNT > 1:
-            from parallel_model import ParallelModel
+            from .parallel_model import ParallelModel
             model = ParallelModel(model, config.GPU_COUNT)
 
         return model
